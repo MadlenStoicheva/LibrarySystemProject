3ab0400c27e144acfd999e35b64832ae2c4ea4d9
MadlenStoicheva <infinite.love@abv.bg>
2018-02-27 18:37:09 +0200

    Initial committ0

--- Data.Entity/Migrations/201802271636203_Initial.Designer.cs
+++ Data.Entity/Migrations/201802271636203_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Entity.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class Automatic : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Automatic));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201802271623242_Automatic"; }
+            get { return "201802271636203_Initial"; }
         }
         
         string IMigrationMetadata.Source
